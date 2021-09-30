7713b002a18e3af027363a0a11d481bf8d9af43b
susie <shuxinli0512@outlook.com>
2021-09-29 20:35:50 -0700

    Checking Concatenation of Array leetcode

--- Array/ConcatenationOfArray/src/com/company/Main.java
+++ Array/ConcatenationOfArray/src/com/company/Main.java
@@ -18,7 +18,8 @@ public class Main {
         int[] ans = new int[nums.length * 2];
         for (int i = 0; i < nums.length; i++)
         {
-            ans[i] = ans[i + nums.length] = nums[i];
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
 
         }
         System.out.println(Arrays.toString(ans));
