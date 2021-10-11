83953f69447c7cf45c049ba3667ff2f603c42bcb
susie <shuxinli0512@outlook.com>
2021-10-11 15:30:42 -0700

    Checking move zeros

--- Array/MoveZeroes/src/com/company/Main.java
+++ Array/MoveZeroes/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,11 +9,29 @@ public class Main {
 	// write your code here
         int[] nums1 = {0,1,0,3,12};
         int[] nums2 = {0};
+        moveZeroes(nums1);
+        moveZeroes(nums2);
 
     }
 
     public static void moveZeroes(int[] nums) {
+        int index = 0;
+        if(nums == null || nums.length == 0){
+            return;
+        }
 
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        for(int i = index; i < nums.length; i++){
+            nums[i] = 0;
+
+        }
+        System.out.println(Arrays.toString(nums));
 
     }
 }
