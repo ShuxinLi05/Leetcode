053d2dd8dc9080d7da9cb1c7bb7296968f1abf33
susie <shuxinli0512@outlook.com>
2021-10-12 15:08:20 -0700

    checking reverse words in a string

--- String/ReverseWordsInAString/src/com/company/Main.java
+++ String/ReverseWordsInAString/src/com/company/Main.java
@@ -8,10 +8,25 @@ public class Main {
         String s3 = "a good   example";
         String s4 = "  Bob    Loves  Alice   ";
         String s5 = "Alice does not even like bob";
+        System.out.println(reverseWords(s1));
+        System.out.println(reverseWords(s2));
+        System.out.println(reverseWords(s3));
+        System.out.println(reverseWords(s4));
+        System.out.println(reverseWords(s5));
 
     }
     public static String reverseWords(String s) {
+        if(s == null){
+            return null;
+        }
+        StringBuilder result = new StringBuilder();
+        String[] part = s.trim().split("\\s+");
 
+        for(int i = part.length - 1; i >= 0; i--){
+            result.append(part[i] + " ");
+        }
+        //System.out.println(result.toString().trim());
+        return result.toString().trim();
 
     }
 }
