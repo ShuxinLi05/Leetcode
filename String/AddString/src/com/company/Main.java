c8dd9ac58853b5dca81a7a4124e75c7944c44a2a
susie <shuxinli0512@outlook.com>
2021-10-11 22:37:09 -0700

    checking add string

--- String/AddString/src/com/company/Main.java
+++ String/AddString/src/com/company/Main.java
@@ -6,27 +6,38 @@ public class Main {
 
         String num1 = "11";
         String num2 = "123";
+        System.out.println(addStrings(num1,num2));
 
         String num3 = "456";
         String num4 = "77";
+        System.out.println(addStrings(num3,num4));
 
         String num5 = "0";
         String num6 = "0";
+        System.out.println(addStrings(num5,num6));
 
     }
 
     public static String addStrings(String num1, String num2) {
 
-        int m = num1.length();
-        int n = num2.length();
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        int i1 = num1.length()-1;
+        int i2 = num2.length()-1;
 
-        if(num1 == null || num2 == null){
-            return null;
+        while (carry >0 || i1 >= 0 || i2 >= 0){
+            int v1 = (i1 >= 0) ? num1.charAt(i1) - '0' : 0;
+            int v2 = (i2 >= 0) ? num2.charAt(i2) - '0' : 0;
+            int sum = v1 + v2 + carry;
+            int value = sum % 10;
+            carry = sum / 10;
+            result.append(value);
+
+            i1 --;
+            i2 --;
+
         }
-
-
-
-        return ;
+        return result.reverse().toString();
     }
 
 }
