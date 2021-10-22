ee2e403ca7344797f50423aa57c13b82bb5d40fd
susie <shuxinli0512@outlook.com>
2021-10-22 15:09:13 -0700

    checking remove duplicate from sorted array

---
+++ Array/RemoveDuplicateFromSortedArray/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,1,2};
+        int[] nums2 = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(nums1));
+        System.out.println(removeDuplicates(nums2));
+
+    }
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int p1 = 0;
+        for (int p2 = 1; p2 < nums.length; p2++){
+            if(nums[p2]!= nums[p1]){
+                p1++;
+                nums[p1] = nums[p2];
+            }
+        }
+        return p1 + 1;
+    }
+
+}
