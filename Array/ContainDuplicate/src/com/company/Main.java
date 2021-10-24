2568d78513ae40505e3b687129d0c2c6afac3bd1
susie <shuxinli0512@outlook.com>
2021-10-23 22:27:42 -0700

    checking contain duplicate

---
+++ Array/ContainDuplicate/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,1};
+        int[] nums2 = {1,2,3,4};
+        int[] nums3 = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println(containsDuplicate(nums1)); //true
+        System.out.println(containsDuplicate(nums2)); //false
+        System.out.println(containsDuplicate(nums3)); //true
+
+    }
+    public static boolean containsDuplicate(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return false;
+        }
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
