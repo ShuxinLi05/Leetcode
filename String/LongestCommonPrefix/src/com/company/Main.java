822959a4b03aab3331ef7c787c1448bea465e93b
susie <shuxinli0512@outlook.com>
2021-10-23 19:37:17 -0700

    checking longest common prefix

---
+++ String/LongestCommonPrefix/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] strs1 = {"flower","flow","flight"};
+        String[] strs2 = {"dog","racecar","car"};
+        System.out.println(longestCommonPrefix(strs1)); // fl
+        System.out.println(longestCommonPrefix(strs2)); //
+
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0){
+            return null;
+        }
+        String prefix = strs[0];
+        for(int i = 1; i < strs.length; i++){
+            while (strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0,prefix.length()-1);
+
+            }
+        }
+        return prefix;
+    }
+}
