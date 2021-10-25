0e110034d99003c9c916a662ed39a93809dfb3ce
susie <shuxinli0512@outlook.com>
2021-10-24 19:52:25 -0700

    checking leetcode 628

--- Array/MaxProdOfThreeNumbs/src/com/company/Main.java
+++ Array/MaxProdOfThreeNumbs/src/com/company/Main.java
@@ -8,9 +8,9 @@ public class Main {
         int[] nums1 = {1,2,3};
         int[] nums2 = {1,2,3,4};
         int[] nums3 = {-1,-2,-3};
-        System.out.println(maximumProduct(nums1));
-        System.out.println(maximumProduct(nums2));
-        System.out.println(maximumProduct(nums3));
+        System.out.println(maximumProduct(nums1)); //6
+        System.out.println(maximumProduct(nums2)); //24
+        System.out.println(maximumProduct(nums3)); //-6
     }
     public static int maximumProduct(int[] nums) {
 
